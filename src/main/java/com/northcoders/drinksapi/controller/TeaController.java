ef56b986b33302764a196ea83bc3dad3ece036b9
Despina Ait-Amer <despinaaitamer@Despinas-MacBook-Air.local>
2024-08-29 16:39:45 +0100

    feat: add logic to Tea class and TeaController

--- src/main/java/com/northcoders/drinksapi/controller/TeaController.java
+++ src/main/java/com/northcoders/drinksapi/controller/TeaController.java
@@ -1,4 +1,24 @@
 package com.northcoders.drinksapi.controller;
 
+import com.northcoders.drinksapi.models.Coffee;
+import com.northcoders.drinksapi.models.Tea;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
 public class TeaController {
+
+    AtomicLong counter = new AtomicLong(0);
+
+    @GetMapping("/tea")
+    public Tea getMoreTea(@RequestParam(value = "name", defaultValue = "yorkshire") String name){
+        long idCounter = counter.incrementAndGet();
+        return new Tea(idCounter, name);
+    }
+
+
 }
+
--- src/test/java/controller/TeaControllerTests.java
+++ src/test/java/controller/TeaControllerTests.java
@@ -1,23 +1,28 @@
-//package controller;
-//
-//import com.northcoders.drinksapi.DrinksapiApplication;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-//
-//@AutoConfigureMockMvc
-//@SpringBootTest(classes = DrinksapiApplication.class)
-//
-//public class TeaControllerTests {
-//
-//    @Test
-//    public void testCoffeeDefaultName() throws Exception {
-//        mockMvc.perform(MockMvcRequestBuilders.get("/coffee"))
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("latte"))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1));
-//    }
-//
-//}
+package controller;
+
+import com.northcoders.drinksapi.DrinksapiApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@AutoConfigureMockMvc
+@SpringBootTest(classes = DrinksapiApplication.class)
+
+public class TeaControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testTeaDefaultName() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/tea"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("yorkshire"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1));
+    }
+
+}
